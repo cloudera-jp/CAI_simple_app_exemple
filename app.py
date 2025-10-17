2f0e51e4fa552bf1815f83aa049bcecfe067562e
tsugiyama-cloudera <134357978+tsugiyama-cloudera@users.noreply.github.com>
2025-10-17 14:35:07 +0900

    add yarn principal setting this is needed to access external text table via hive/yarn

--- app.py
+++ app.py
@@ -8,11 +8,22 @@ from pyspark.sql.types import *
 
 app = Flask(__name__)
 
+# KERBEROSレルムの取得
+CDSW_DOMAIN = os.environ.get('CDSW_DOMAIN')
+if CDSW_DOMAIN:
+    parts = CDSW_DOMAIN.split('.')
+    realm_low = '.'.join(parts[1:])
+    realm = realm_low.upper()
+
+# YARNプリンシパル文字列の生成
+yarn_realm = "yarn/_HOST@" + realm
+
 # Sparkセッションの初期化
-spark = SparkSession.builder.appName("PythonSQL").master("local[*]").getOrCreate()
+spark = SparkSession.builder.appName("PythonSQL").master("local[*]").config("yarn.resourcemanager.principal",yarn_realm).getOrCreate()
 
 # 初期表示データの読み込み
-df = spark.sql("SELECT * FROM simple_table")
+df = spark.sql("SELECT * FROM simple_external")
+# df.createOrReplaceTempView("simple_external")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -23,7 +34,7 @@ def index():
         filter_value = request.form.get("city_filter", "")
         if filter_value:
             # Spark SQLを使ってデータをフィルタリング
-            sql_query = f"SELECT * FROM simple_table WHERE col3 = '{filter_value}'"
+            sql_query = f"SELECT * FROM simple_external WHERE col3 = '{filter_value}'"
             filtered_df = spark.sql(sql_query)
     else:
         filtered_df = df
